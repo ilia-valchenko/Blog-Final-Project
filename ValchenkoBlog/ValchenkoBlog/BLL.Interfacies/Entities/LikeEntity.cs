8c73bba2035e0062bf7d298ea38a0c139b9fa639
Ilia Valchenko <ilia.valchenko.epam@gmail.com>
2016-09-10 23:29:47 +0300

    Remove duplicated directory.

--- ValchenkoBlog/ValchenkoBlog/BLL.Interfacies/Entities/LikeEntity.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BLL.Interfacies.Entities
-{
-    public class LikeEntity
-    {
-        public int Id { get; set; }
-        public int PostId { get; set; }
-        public int UserId { get; set; }
-    }
-}
--- ValchenkoBlog/ValchenkoBlog/DAL.Interfacies/DTO/IEntity.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace DAL.Interfacies.DTO
-{
-    public interface IEntity
-    {
-        int Id { get; set; }
-    }
-}
