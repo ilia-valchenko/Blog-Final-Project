82c69255944871d6798bb08d65584c98d66be438
Ilia Valchenko <ilia.valchenko.epam@gmail.com>
2016-09-14 22:31:19 +0300

    Fix some problems with deleting comments via ajax.

--- ValchenkoBlog/ValchenkoBlog/MvcPL/Scripts/Custom/comment.js
+++ ValchenkoBlog/ValchenkoBlog/MvcPL/Scripts/Custom/comment.js
@@ -14,32 +14,64 @@
                 dataType: 'json',
                 success: function (result) {
 
-                    var file = base64js.fromByteArray(jQuery.makeArray(result.User.Avatar);
+                    var file = base64js.fromByteArray(jQuery.makeArray(result.User.Avatar));
 
-                    $("#commentwrapper").append('<div class="comment">' +
+                    $("#commentwrapper").append('<div class="comment" id="comment' + result.Id + '">' +
                         '<div class="avatar-wrapper">' +
                             '<img src="data:image/png;base64,' + file + '" alt="' + result.User.Nickname + '_avatar_image' + '">' +
-                        '</div>' +
+                        '</div>' + 
                             '<div class="comment-area">' + 
                                 '<p><strong>' + result.User.Nickname + '</strong><span>' + result.PublishDate + '</span></p>' +
                                 '<p>' + result.Text + '</p>' + 
                                 '<form method="post" id="comment-delete-form" action="/Post/DeleteComment">' + 
                                     '<input type="hidden" id="commentid" name="id" ' + 'value=' + result.Id + '>' +
-                                    '<input type="submit" value="Delete">' +
+                                    '<input type="submit" class="dynamic_element_submit" value="Delete">' +
                                 '</form>' +
                             '</div>' +
                         '</div>');
-                    
+
                     $("#number_of_comments").html(parseInt($("#number_of_comments").text()) + 1);
+                    $("#text").val('');
+
+                    $("#comment-delete-form").submit(function (event) {
+
+                        event.preventDefault();
+                        alert("Click by submit button");
+
+                        var id = $(this).find("#commentid").val();
+
+                        $.ajax({
+                            type: 'POST',
+                            url: "/Post/DeleteCommentViaAjax",
+                            data: {
+                                'id': id
+                            },
+                            dataType: 'json',
+                            success: function (result) {
+                                alert("Successfully deleted!");
+                                alert($("#comment" + id));
+                                $("#comment" + id).remove();
+                                alert("After deleting");
+                                $("#number_of_comments").html(parseInt($("#number_of_comments").text()) - 1);
+                            },
+                            async: true,
+                        });
+
+                    });
+
+                    
                 },
                 async: true,
             });
 
     });
 
+    // Delete comment
+
     $("#comment-delete-form").submit(function (event) {
-        
+
         event.preventDefault();
+
         var id = $(this).find("#commentid").val();
 
         $.ajax({
@@ -59,6 +91,7 @@
     });
 
 });
+
 
 
  
--- ValchenkoBlog/ValchenkoBlog/ORM/BlogDbContext.cs
+++ ValchenkoBlog/ValchenkoBlog/ORM/BlogDbContext.cs
@@ -3,6 +3,11 @@ using ORM.Models;
 
 namespace ORM
 {
+    /// <summary>
+    /// A DbContext instance represents a combination of the Unit Of Work and Repository patterns such 
+    /// that it can be used to query from a database and group together changes that will then 
+    /// be written back to the store as a unit.
+    /// </summary>
     public class BlogDbContext : DbContext
     {
         static BlogDbContext()
@@ -10,8 +15,15 @@ namespace ORM
             Database.SetInitializer(new DbInitializer());
         }
 
+        /// <summary>
+        /// Default constructor. 
+        /// It refers to the base constructor, passing to it the connection string.
+        /// </summary>
         public BlogDbContext() : base("name=BlogDbContext") {}
 
+        /// <summary>
+        /// 
+        /// </summary>
         public virtual DbSet<User> Users { get; set; }
         public virtual DbSet<Role> Roles { get; set; }
         public virtual DbSet<Post> Posts { get; set; }
